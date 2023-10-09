16b1f598edc40bc5957ff8bac28783f08f4b9b21
Jean Lacerda <jean.sena2@gmail.com>
2023-10-09 20:53:20 -0300

    desafio validações em product

--- src/domain/product/entity/product.ts
+++ src/domain/product/entity/product.ts
@@ -1,30 +1,28 @@
+import Entity from "../../@shared/entity/entity.abstract";
+import NotificationError from "../../@shared/notification/notification.error";
+import ProductValidatorFactory from "../factory/product.validator.factory";
 import ProductInterface from "./product.interface";
 
-export default class Product implements ProductInterface{
+export default class Product extends Entity implements ProductInterface{
 
-    private _id: string;
     private _name: string;
     private _price: number;
 
 	constructor(id: string, name: string, price: number) {
+        super();
 		this._id = id;
 		this._name = name;
 		this._price = price;
         this.validate();
+
+        if(this.notification.hasErrors()){
+			throw new NotificationError(this.notification.getErrors());
+		}
     }
 
     validate():boolean{
        
-        if(this._id.length === 0 ){
-            throw new Error("Id is required");
-        }
-
-        if(this._name.length === 0){
-            throw new Error("Name is required");
-        }
-        if(this._price < 0){
-            throw new Error("Price must be greater than zero");
-        }
+        ProductValidatorFactory.create().validate(this);
         return true;
     }
 
